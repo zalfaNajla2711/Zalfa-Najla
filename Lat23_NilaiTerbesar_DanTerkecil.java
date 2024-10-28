f7693618c00c39c5d15867752b9b699e13843772
zalfaNajla2711 <zalfadhianajla@gmail.com>
2024-10-28 20:54:22 +0700

    Add files via upload

---
+++ Lat23_NilaiTerbesar_DanTerkecil.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lat23_nilaiterbesar_danterkecil;
+
+/**
+ *
+ * @author hp5cd
+ */
+import java.util.Scanner;
+
+public class Lat23_NilaiTerbesar_DanTerkecil {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=====Program Nilai Terbesar dan Terkecil Nilai Mahasiswa=====");
+        
+        // Input nama petugas
+        System.out.print("Masukkan Nama Petugas : ");
+        String namaPetugas = scanner.nextLine();
+
+        // Input banyaknya nilai mahasiswa
+        System.out.print("Masukkan Banyaknya Nilai Mahasiswa : ");
+        int jumlahMahasiswa = scanner.nextInt();
+
+        // Deklarasi array untuk menyimpan nilai mahasiswa
+        int[] nilaiMahasiswa = new int[jumlahMahasiswa];
+
+        // Input nilai mahasiswa
+        for (int i = 0; i < jumlahMahasiswa; i++) {
+            System.out.print("Masukkan Nilai Mahasiswa Ke-" + (i + 1) + " = ");
+            nilaiMahasiswa[i] = scanner.nextInt();
+        }
+
+        // Inisialisasi nilai terbesar dan terkecil
+        int nilaiTerbesar = nilaiMahasiswa[0];
+        int nilaiTerkecil = nilaiMahasiswa[0];
+
+        // Mencari nilai terbesar dan terkecil
+        for (int i = 1; i < jumlahMahasiswa; i++) {
+            if (nilaiMahasiswa[i] > nilaiTerbesar) {
+                nilaiTerbesar = nilaiMahasiswa[i];
+            }
+            if (nilaiMahasiswa[i] < nilaiTerkecil) {
+                nilaiTerkecil = nilaiMahasiswa[i];
+            }
+        }
+
+        // Menampilkan hasil
+        System.out.println("\n=====Hasil Nilai Mahasiswa=====");
+        for (int i = 0; i < jumlahMahasiswa; i++) {
+            System.out.println("Nilai Mahasiswa Ke-" + (i + 1) + " = " + nilaiMahasiswa[i]);
+        }
+        
+        System.out.println("\nNilai Terbesar adalah " + nilaiTerbesar);
+        System.out.println("Nilai Terkecil adalah " + nilaiTerkecil);
+        System.out.println("\nPetugas : " + namaPetugas);
+
+        scanner.close();
+    }
+}
